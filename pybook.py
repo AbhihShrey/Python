0e0705c15ad9d14537071e3cfefdc9de67d56953
Abhih <abhih.shrey@gmail.com>
2021-02-17 17:48:47 -0800

    Minor commi

---
+++ pybook.py
@@ -0,0 +1,14 @@
+def chapter_11_notes():
+    a = ('This is the first line. \nThis is the second.')
+    print(a)
+    '''
+    Note 1: Before you write to a file, remember that the file will be written in the directory in
+    which you are working unless you give a full path name. To find the name of your working
+    directory under Linux, type pwd (print working directory).
+    '''
+
+
+chapter_11_notes()
+
+def chapter_12_notes():
+    pass

c4b02142c1138e3e20e9a073260b8e97e7130fa2
AbhihShrey <65800085+AbhihShrey@users.noreply.github.com>
2022-07-10 08:20:10 -0700

    Add files via upload
    This is my mad libs program.

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+# Create the adjectives, verbs, and nouns
+verb = input("Enter a verb: ")
+noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
+place = input("Enter a place: ")
+# Put it all together in a story
+
+story = f"Today was a very hard day, I almost had to {verb}! I tried to get along with the other students but they all went to hang out with that other kid, {noun}, jesus I wish I was that {adjective}. Anyways see see you at the {place}"
+
+print(story)
